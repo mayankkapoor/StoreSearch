ca6223f66c1b531283e26cc394819c48dc199828
Mayank Kapoor <mayankkapoormail@gmail.com>
2014-08-24 19:04:11 -0700

    Added SearchBar and TableView to the SearchVC. Connected as delegates and datasource, implemented required protocol methods for TableViewDataSource

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
                             
+	@IBOutlet weak var searchBar: UISearchBar!
+	@IBOutlet weak var tableView: UITableView!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +21,14 @@ class SearchViewController: UIViewController {
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
+	}
+	
+	func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+		return 0
+	}
+	
+	func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+		return nil
 	}
 
 
