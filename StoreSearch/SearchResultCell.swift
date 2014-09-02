2a299dc005a2c5fb8491c29e41bae97b5c01ad60
Mayank Kapoor <mayankkapoormail@gmail.com>
2014-09-02 02:54:10 -0700

    Added background colors and selected background colors.

--- StoreSearch/SearchResultCell.swift
+++ StoreSearch/SearchResultCell.swift
@@ -14,4 +14,16 @@ class SearchResultCell: UITableViewCell {
 	@IBOutlet weak var nameLabel: UILabel!
 	@IBOutlet weak var artistNameLabel: UILabel!
 	@IBOutlet weak var artworkImageView: UIImageView!
+	
+	override func awakeFromNib() {
+		super.awakeFromNib()
+		
+		let image = UIImage(named: "TableCellGradient")
+		let backgroundImageView = UIImageView(image: image)
+		self.backgroundView = backgroundImageView
+		
+		let selectedImage = UIImage(named: "SelectedTableCellGradient")
+		let selectedBackgroundImageView = UIImageView(image: selectedImage)
+		self.selectedBackgroundView = selectedBackgroundImageView
+	}
 }
